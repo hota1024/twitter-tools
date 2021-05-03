4e1ef720cf9e23d2cb881dff55f57f6f39142b89
hota1024 <hotalog@hota1024.com>
2021-05-04 02:15:52 +0900

    ✨ add retweet and like notifier

--- src/ActionNotifier/ActionNotifier.ts
+++ src/ActionNotifier/ActionNotifier.ts
@@ -5,6 +5,7 @@ import { Component } from '@/interfaces/Component'
 import { BOT, EVENTS, STORAGE } from '@/keys'
 import { AppStorage } from '@/Storage/AppStorage'
 import { inject, injectable } from 'tsyringe'
+import { FullUser } from 'twitter-d'
 
 /**
  * ActionNotifier class.
@@ -25,20 +26,76 @@ export class ActionNotifier implements Component {
   }
 
   start(): void {
-    this.events.onFollow(async (e) => {
-      const actionFolowers = (await this.storage.get(
-        'actionFolowers'
-      )) as number
-      const isForUser = e.for_user_id === getEnv('MAIN_ID', true)
-
-      if (!isForUser) {
+    this.events.onTweetCreate(async (e) => {
+      if (!this.isMe(e.for_user_id)) {
         return
       }
+
+      const actionFollowers = await this.getActionFollowers()
+
+      for (const event of e.tweet_create_events) {
+        const isTargetMain = ((event.user as unknown) as FullUser).id_str
+        const hasActionFollowers = event.user.followers_count >= actionFollowers
+        const isRetweet = !!((event as unknown) as {
+          retweeted_status: unknown
+        })['retweeted_status']
+
+        if (isTargetMain && hasActionFollowers && isRetweet) {
+          this.bot.notify(
+            `${
+              event.user.screen_name
+            } さんにリツイートされました(フォロワー数: ${this.bot.inlineCode(
+              event.user.followers_count.toString()
+            )})\nユーザー: https://twitter.com/${
+              event.user.screen_name
+            }\nツイート: https://twitter.com/${event.user.screen_name}/status/${
+              event.id_str
+            }`
+          )
+        }
+      }
+    })
+
+    this.events.onFavorite(async (e) => {
+      if (!this.isMe(e.for_user_id)) {
+        return
+      }
+
+      const actionFollowers = await this.getActionFollowers()
+
+      for (const event of e.favorite_events) {
+        const isTargetMain = ((event.favorited_status
+          .user as unknown) as FullUser).id_str
+        const hasActionFollowers = event.user.followers_count >= actionFollowers
+
+        if (isTargetMain && hasActionFollowers) {
+          this.bot.notify(
+            `${
+              event.user.screen_name
+            } さんにいいねされました(フォロワー数: ${this.bot.inlineCode(
+              event.user.followers_count.toString()
+            )})\nユーザー: https://twitter.com/${
+              event.user.screen_name
+            }\nツイート: https://twitter.com/${event.user.screen_name}/status/${
+              event.favorited_status.id_str
+            }`
+          )
+        }
+      }
+    })
+
+    this.events.onFollow(async (e) => {
+      if (!this.isMe(e.for_user_id)) {
+        return
+      }
+
+      const actionFollowers = await this.getActionFollowers()
 
       for (const event of e.follow_events) {
         const isFollow = event.type === 'follow'
         const isTargetMain = event.target.id === getEnv('MAIN_ID', true)
-        const hasActionFolowers = event.source.followers_count >= actionFolowers
+        const hasActionFolowers =
+          event.source.followers_count >= actionFollowers
 
         if (isFollow && isTargetMain && hasActionFolowers) {
           const user = event.source
@@ -53,6 +110,21 @@ export class ActionNotifier implements Component {
         }
       }
     })
-    return
+  }
+
+  private async getActionFollowers() {
+    const actionFolowers = await this.storage.get('actionFolowers')
+
+    if (!actionFolowers) {
+      throw new Error('[ActionNotifier] actionFollowers is not defined')
+    }
+
+    return actionFolowers
+  }
+
+  private isMe(id: string) {
+    const isForUser = id === getEnv('MAIN_ID', true)
+
+    return isForUser
   }
 }
