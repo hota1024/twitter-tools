2d19b7e33099c2bdc290cb7528779e0b1c794edd
hota1024 <hotalog@hota1024.com>
2021-05-08 23:58:47 +0900

    🐛 fix auto follow next follow date at

--- src/AutoFollow/AutoFollow.ts
+++ src/AutoFollow/AutoFollow.ts
@@ -5,6 +5,7 @@ import { waitFor } from '@/helpers/waitFor'
 import { Component } from '@/interfaces/Component'
 import { EVENTS, STORAGE, BOT } from '@/keys'
 import { AppStorage } from '@/Storage/AppStorage'
+import { CronJob } from 'cron'
 import { inject, injectable } from 'tsyringe'
 import Twitter from 'twitter'
 import { FullUser } from 'twitter-d'
@@ -53,7 +54,85 @@ export class AutoFollow implements Component {
       }
     })
 
-    this.updateQueue()
+    const date = new Date()
+
+    if (date.getHours() > 12) {
+      date.setDate(date.getDate() + 1)
+    } else {
+      date.setHours(12)
+      date.setMinutes(0)
+      date.setSeconds(0)
+    }
+
+    this.storage.set('nextFollowAt', date.getTime())
+
+    new CronJob('12 0 * * *', () => {
+      this.processQueue(200)
+
+      const date = new Date()
+      date.setDate(date.getDate() + 1)
+      date.setHours(12)
+      date.setMinutes(0)
+      date.setSeconds(0)
+      this.storage.set('nextFollowAt', date.getTime())
+    })
+  }
+
+  private async processQueue(count: number) {
+    for (
+      let i = 0;
+      i <
+      Math.min(
+        count,
+        (await this.storage.get('followQueue'))?.length ?? Infinity
+      );
+      ++i
+    ) {
+      if (!(await this.storage.get('enableAutoFollow'))) {
+        break
+      }
+
+      const queue = await this.storage.get('followQueue')
+      if (!queue) {
+        console.error('[AUTO_FOLLOW] followQueue is not defined')
+        break
+      }
+
+      if (!queue[i]) {
+        console.error(`[AUTO_FOLLOW] queue[${i}] is not defined`)
+        break
+      }
+
+      const id = queue.shift()
+      const account = await this.twitter
+        .get('/users/show.json', {
+          user_id: id,
+        })
+        .then((v) => v as FullUser)
+        .catch((e) => {
+          console.error('[BOT]', e)
+
+          return {
+            screen_name: void 0,
+          }
+        })
+
+      const accountName = account.screen_name || '`unknown`'
+
+      try {
+        await this.twitter.post('/friendships/create.json', {
+          user_id: id,
+        })
+        this.bot.notify(
+          `サブアカウントで ${accountName} さんをフォローしました。\nhttps://twitter.com/${accountName}`
+        )
+        await this.storage.set('followQueue', queue)
+      } catch {
+        this.bot.notify(
+          `${accountName} さんのフォローに失敗しました。\nhttps://twitter.com/${accountName}`
+        )
+      }
+    }
   }
 
   private async isMe(id: string) {
@@ -93,62 +172,6 @@ export class AutoFollow implements Component {
         console.error('[BOT]', e)
       }
     }
-  }
-
-  private async updateQueue() {
-    if (!this.bot.notifyChannel) {
-      this.requestUpdateQueue(1000)
-      return
-    }
-
-    const queue = await this.storage.get('followQueue')
-
-    if (typeof queue === 'undefined') {
-      throw this.createFollowQueueUndefinedError()
-    }
-
-    if (queue.length === 0) {
-      this.requestUpdateQueue(1000)
-      return
-    }
-
-    const id = queue.shift()
-    const account = await this.twitter
-      .get('/users/show.json', {
-        user_id: id,
-      })
-      .then((v) => v as FullUser)
-      .catch((e) => {
-        console.error('[BOT]', e)
-
-        return {
-          screen_name: void 0,
-        }
-      })
-
-    const accountName = account.screen_name || '`unknown`'
-
-    try {
-      await waitFor(216000000)
-      await this.twitter.post('/friendships/create.json', {
-        user_id: id,
-      })
-      this.bot.notify(
-        `サブアカウントで ${accountName} さんをフォローしました。\nhttps://twitter.com/${accountName}`
-      )
-      await this.storage.set('followQueue', queue)
-      this.requestUpdateQueue(216000)
-    } catch {
-      this.bot.notify(
-        `${accountName} さんのフォローに失敗しました。\nhttps://twitter.com/${accountName}`
-      )
-      this.requestUpdateQueue(60000)
-    }
-  }
-
-  private requestUpdateQueue(timeout: number) {
-    this.storage.set('nextFollowAt', Date.now() + timeout)
-    setTimeout(() => this.updateQueue(), timeout)
   }
 
   /**
